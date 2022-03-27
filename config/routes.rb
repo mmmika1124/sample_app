bc60044c83594f3b5206f00e170539159ccb606f
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-03-27 11:39:02 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'top' => 'home#top'
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
